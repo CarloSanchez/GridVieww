45b1893377ff1aa6280e7787ee470589bc70a03d
Carlos <sancar9313@gmail.com>
2015-09-10 10:47:10 -0500

    listo

---
+++ app/src/main/java/com/example/carlos/gridvieww/ImageAdapter.java
@@ -0,0 +1,60 @@
+package com.example.carlos.gridvieww;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+
+/**
+ * Created by Carlos on 10/09/2015.
+ */
+public class ImageAdapter extends BaseAdapter{
+
+    private Context mContext;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+    // references to our images
+    private Integer[] mThumbIds = {
+
+            R.drawable.hjdsh232_34,
+            R.drawable.dffe_676,
+            R.drawable.dfsdf_656,
+            R.drawable.ghfgh_545,
+            R.drawable.sfg_664
+    };
+}
